56436d8d0d7784f6f98d6b3856a75890063aedfb
TStutsman <teagandstutsman@gmail.com>
2024-04-24 19:07:50 -0400

    add: channel heading and members

--- react-vite/src/components/ChannelPanel/ChannelPanel.jsx
+++ react-vite/src/components/ChannelPanel/ChannelPanel.jsx
@@ -1,14 +1,17 @@
 import './ChannelPanel.css'
 
 function ChannelPanel() {
+    const messages = [1, 2, 3, 4, 5]
     return (
         <div id="channel-panel">
             <div id='channel-details'>
                 <h3>Channel Name</h3>
+                <p>369 members</p>
             </div>
             <div id='message-feed'>
-                <h1>Welcome to ChannelPanel</h1>
-
+                { messages.map(message => (
+                    <p key={message}>Message {message}</p>
+                ))}
             </div>
         </div>
     )
