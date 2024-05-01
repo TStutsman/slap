dfbf995468b4697c2e0ac8a8fc902e99f8a84ad8
TStutsman <teagandstutsman@gmail.com>
2024-05-01 11:41:08 -0400

    add: alert on dm click

--- react-vite/src/components/DMList/DMList.jsx
+++ react-vite/src/components/DMList/DMList.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { FaCaretDown } from 'react-icons/fa6'
 import { NavLink } from "react-router-dom";
 import { getAllUsersThunk } from "../../redux/users";
+import './DMList.css';
 
 function DMList() {
     const dispatch = useDispatch();
@@ -16,15 +17,17 @@ function DMList() {
     if(!users.byId) return null;
 
     return (
-        <div id="dm-list">
+        <div id="dm-list-wrapper">
             <div className='list-controls no-select'>
                 <FaCaretDown className={showDMs ? "caret" : "caret rotate"} onClick={() => setShowDMs(!showDMs)}/>
                 <p>Direct Messages</p>
-                <button className="options-control">...</button>
+                <button className="options-control" onClick={() => alert('Feature coming soon!')}>...</button>
             </div>
-            { showDMs && Object.entries(users.byId).map(([id, user]) => (
-                <NavLink key={id} to="/">{user.firstName} {user.lastName}</NavLink>
-            ))}
+            <div id="dm-list" onClick={() => alert("Feature coming soon!")}>
+                { showDMs && Object.entries(users.byId).map(([id, user]) => (
+                    <NavLink key={id} to="/">{user.firstName} {user.lastName}</NavLink>
+                ))}
+            </div>
         </div>
     )
 }
