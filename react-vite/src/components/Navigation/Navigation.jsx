b9dbecf082d7c2f81ee14bd0c87c349690f8a894
TStutsman <teagandstutsman@gmail.com>
2024-05-07 19:20:02 -0400

    add: about-links for greenlit

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,3 +1,4 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa6'
 import Profile from "./Profile";
 import ChannelList from "../ChannelList";
 import DMList from "../DMList";
@@ -9,6 +10,10 @@ function Navigation() {
       <Profile />
       <ChannelList />
       <DMList />
+      <div id="about-links">
+        <a href='https://github.com/TStutsman' target='_blank' rel='noreferrer'><FaGithub /> TStutsman</a>
+        <a href='https://www.linkedin.com/in/teagan-stutsman-22a0a4237/' target='_blank' rel='noreferrer'><FaLinkedin /> Teagan Stutsman</a>
+      </div>
     </nav>
   );
 }
