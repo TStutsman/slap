db8d46c26338184169d8026d45d510f4a6a2d4f9
TStutsman <teagandstutsman@gmail.com>
2024-05-21 11:46:17 -0400

    add: loading screen on user authorization

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { thunkLogin } from "../../redux/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
+import Loading from '../Loading';
 import "./LoginForm.css";
 
 function LoginFormModal() {
@@ -11,8 +12,11 @@ function LoginFormModal() {
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
 
+  const [loading, setLoading] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true); // Tell the user we're waiting for the server
 
     const serverResponse = await dispatch(
       thunkLogin({
@@ -21,6 +25,7 @@ function LoginFormModal() {
       })
     );
 
+    setLoading(false);
     if (serverResponse) {
       setErrors(serverResponse);
     } else {
@@ -28,38 +33,46 @@ function LoginFormModal() {
     }
   };
 
-  const demoLogin = () => {
-    dispatch(thunkLogin({ email:'demo@aa.io', password:'password' }));
+  const demoLogin = async () => {
+    setLoading(true);
+    await dispatch(thunkLogin({ email:'demo@aa.io', password:'password' }));
+    setLoading(false);
     closeModal();
   }
 
   return (
     <div className="login-modal">
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p className="error">{errors.email}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p className="error">{errors.password}</p>}
-        <button type="submit">Log In</button>
-      </form>
-      <button className="demo" onClick={demoLogin}>Demo User</button>
+      { loading ? 
+        <Loading />
+      :
+      <>
+        <h1>Log In</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+          {errors.email && <p className="error">{errors.email}</p>}
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          {errors.password && <p className="error">{errors.password}</p>}
+          <button type="submit">Log In</button>
+        </form>
+        <button className="demo" onClick={demoLogin}>Demo User</button>
+      </>
+      }
     </div>
   );
 }
