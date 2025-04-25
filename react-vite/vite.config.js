6a0d17c38ab53d31d7aea527a7f691eafe2b1d6f
TStutsman <teagandstutsman@gmail.com>
2025-04-25 09:17:17 -0500

    vite aliasing

--- react-vite/vite.config.js
+++ react-vite/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig((mode) => ({
@@ -17,4 +18,14 @@ export default defineConfig((mode) => ({
       "/api": "http://127.0.0.1:8000",
     },
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@redux': path.resolve(__dirname, './src/redux'),
+      '@router': path.resolve(__dirname, './src/router'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    }
+  }
 }));
